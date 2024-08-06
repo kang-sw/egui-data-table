a81b52f4b411e551520d3c19611ce2d9ca5a369d
kang-sw <ki6080@gmail.com>
2024-08-06 09:40:51 +0900

    demo: fix weird zooming bug
    * Just pasted official web demo code.

--- examples/demo.rs
+++ examples/demo.rs
@@ -307,16 +307,36 @@ fn main() {
 
 #[cfg(target_arch = "wasm32")]
 fn main() {
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
     let web_options = eframe::WebOptions::default();
 
     wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
+        let start_result = eframe::WebRunner::new()
             .start(
-                "egui_data_table_demo",
+                "the_canvas_id",
                 web_options,
-                Box::new(|_| Ok(Box::new(DemoApp::default()))),
+                Box::new(|cc| Ok(Box::new(DemoApp::default()))),
             )
-            .await
-            .expect("failed to start eframe");
+            .await;
+
+        // Remove the loading text and spinner:
+        let loading_text = web_sys::window()
+            .and_then(|w| w.document())
+            .and_then(|d| d.get_element_by_id("loading_text"));
+        if let Some(loading_text) = loading_text {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
     });
 }
